27d13ef69169b88c430f33c266223110385ee07f
Lorwik <lorwik@gmail.com>
2020-08-14 21:36:53 +0200

    Termno tema 2 y 3

---
+++ 03-pipes/src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  nombre: string = 'Manuel';
+  nombre2: string = 'MaNuEl JeSuS SaNdAlIo';
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+  fecha: Date = new Date();
+  activar: boolean = true;
+
+  idioma: string ="es";
+  videoUrl: string = 'https://www.youtube.com/embed/2j0OPN3-u3s';
+
+  valorPromesa = new Promise<string>( (resolve) => { 
+
+  setTimeout(() => {
+    resolve('llego la data');
+  }, 4500);
+
+  });
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  }
+
+}
---
+++ 03-pipes/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
+
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeEs);
+registerLocaleData(localeFr);
+
+import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
